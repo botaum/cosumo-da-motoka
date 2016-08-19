87c2d9c8db2fcf78314aeb23b238581b96c84f68
botaum <rbdf91@gmail.com>
2016-08-19 18:18:11 -0300

    Inclusao da segunda activity, resta programar os calculos

--- app/src/main/java/com/consumodamotoka/MainActivity.java
+++ app/src/main/java/com/consumodamotoka/MainActivity.java
@@ -1,13 +1,25 @@
 package com.consumodamotoka;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public final static String EXTRA_MESSAGE = "com.consumodamotoka.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**Chamada para o método que cálcula as informações relevantes de consumo da Motoka*/
+    public void calcularInformacao(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText)findViewById(R.id.litros_de_combustivel);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
