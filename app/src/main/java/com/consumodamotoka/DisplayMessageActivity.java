a65ede67cbd1191ab6d9ed060e665e910252acd0
botaum <rbdf91@gmail.com>
2016-08-20 09:37:09 -0300

    Comitei os calculos de total a pagar e media por litro, falta recuperar os dados de data para gerar quilometros por dia e gravar em arquivo, além de trabalhar mais com interface

--- app/src/main/java/com/consumodamotoka/DisplayMessageActivity.java
+++ app/src/main/java/com/consumodamotoka/DisplayMessageActivity.java
@@ -12,15 +12,37 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
-
+        /** recuperar as informações passada pela activity que chamou */
         Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        TextView textView = new TextView(this);
-        textView.setTextSize(40);
-        textView.setText(message);
+        /** recuperar a informação de dentro da intent */
+        String litros_de_combustivel = intent.getStringExtra(MainActivity.LITROS_DE_COMBUSTIVEL);
+        String preco_do_litro_de_combustivel = intent.getStringExtra(MainActivity.PRECO_DO_LITRO_DE_COMBUSTIVEL);
+        String quilometros_rodados = intent.getStringExtra(MainActivity.QUILOMETROS_RODADOS);
+        /** aqui estamos gerando uma textView dinamicamente em tempo de execução, talvez o ideal seja criar no xml de layout */
+        TextView dados_de_entrada = (TextView)findViewById(R.id.dados_de_entrada);
+        dados_de_entrada.setText(String.format("Você colocou: %s litros" +
+                                       "\nO preço do litro foi: R$ %s" +
+                                       "\nE você rodou: %s " + " quilômetros",
+                                       litros_de_combustivel,
+                                       preco_do_litro_de_combustivel,
+                                       quilometros_rodados));
+        /** vou tentar gerar uma nova textView e adicionar ela com os resultados do calculo de KM/l, KM/dia, total a pagar */
+        Float litros = Float.parseFloat(litros_de_combustivel);
+        Float preco = Float.parseFloat(preco_do_litro_de_combustivel);
+        Float quilometros = Float.parseFloat(quilometros_rodados);
 
-        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
-        layout.addView(textView);
+        Float totalAPagar = litros * preco;
+        Float quilometrosPorLitro = quilometros / litros;
+
+        TextView resultados = (TextView)findViewById(R.id.resultados);
+        /** resultados.setTextSize(26); */
+        resultados.setText(String.format("O valor total a pagar é: R$%s" +
+                                         "\nA quilometragem média por litro foi de: %s quilometros",
+                                         totalAPagar,
+                                         quilometrosPorLitro));
+
+        /** layout = (ViewGroup) findViewById(R.id.activity_display_message);
+        layout.addView(resultados); */
 
     }
 }
--- app/src/main/java/com/consumodamotoka/MainActivity.java
+++ app/src/main/java/com/consumodamotoka/MainActivity.java
@@ -7,7 +7,9 @@ import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    public final static String EXTRA_MESSAGE = "com.consumodamotoka.MESSAGE";
+    public final static String LITROS_DE_COMBUSTIVEL = "com.consumodamotoka.LITROS_DE_COMBUSTIVEL";
+    public final static String PRECO_DO_LITRO_DE_COMBUSTIVEL = "com.consumodamotoka.PRECO_DO_LITRO_DE_COMBUSTIVEL";
+    public final static String QUILOMETROS_RODADOS = "com.consumodamotoka.QUILOMETROS_RODADOS";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,10 +18,23 @@ public class MainActivity extends AppCompatActivity {
 
     /**Chamada para o método que cálcula as informações relevantes de consumo da Motoka*/
     public void calcularInformacao(View view){
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText)findViewById(R.id.litros_de_combustivel);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+        /** criar nova Inteção de fazer algo: */
+        Intent intent = new Intent(MainActivity.this, DisplayMessageActivity.class);
+        /** aqui a gente cria uma variavel para o editText */
+        EditText editText_litros_de_combustivel = (EditText)findViewById(R.id.litros_de_combustivel);
+        /** recuperar o texto que está contido no editText */
+        String litros_de_combustivel = editText_litros_de_combustivel.getText().toString();
+        /** atribui a variavel que será passada pelo intent*/
+        intent.putExtra(LITROS_DE_COMBUSTIVEL, litros_de_combustivel);
+        /** repete o que fizemos para o litros, só que agora para o preço do combustível */
+        EditText editText_preco_do_litro_de_combustivel = (EditText)findViewById(R.id.preco_do_litro_de_combustivel);
+        String preco_do_litro_de_combustivel = editText_preco_do_litro_de_combustivel.getText().toString();
+        intent.putExtra(PRECO_DO_LITRO_DE_COMBUSTIVEL,preco_do_litro_de_combustivel);
+        /** repete o que fizemos para os quilometros rodados */
+        EditText editText_quilometros_rodados = (EditText)findViewById(R.id.quilometros_rodados);
+        String quilometros_rodados = editText_quilometros_rodados.getText().toString();
+        intent.putExtra(QUILOMETROS_RODADOS,quilometros_rodados);
+        /** inicializa a nova Activity passando tudo que foi colocado no intent */
         startActivity(intent);
     }
 }
